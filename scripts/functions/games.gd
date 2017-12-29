4059ca4684e5bb222329b27c4ad0d6212d0486ce
CzlowiekImadlo <quak@tlen.pl>
2017-12-29 11:56:15 +0100

    Added basic game scene

--- scripts/functions/games.gd
+++ scripts/functions/games.gd
@@ -2,8 +2,15 @@ extends "res://scripts/bag_aware.gd"
 
 const LIST_FILENAME = "games.list"
 
-var games = ["test"]
+var games = []
 
 func _initialize():
     self.games = self.bag.file_handler.read_text(self.LIST_FILENAME)
-    print(self.games)
+    print(self.games.size())
+
+func get(index):
+    if index < 0 or index > self.games.size():
+        return null
+
+    return self.games[index]
+
