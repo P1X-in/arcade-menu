51151dc641e13a99cb8b3fde5dff408f4ad9ae91
CzlowiekImadlo <quak@tlen.pl>
2017-12-29 15:57:35 +0100

    Added methods for configuring input

--- scripts/services/input/input.gd
+++ scripts/services/input/input.gd
@@ -1,5 +1,11 @@
 extends "res://scripts/services/bag_aware.gd"
 
+const DEVICE_KEYBOARD = 'keyboard'
+const DEVICE_GAMEPAD = 'keyboard'
+const DEVICE_MOUSE = 'mouse'
+const DEVICE_ARCADE = 'arcade'
+const DEVICE_ANY = 'any'
+
 
 var keyboard_template = preload("res://scripts/services/input/keyboard.gd")
 var gamepad_template = preload("res://scripts/services/input/gamepad.gd")
@@ -8,16 +14,15 @@ var arcade_template = preload("res://scripts/services/input/arcade.gd")
 var any_device_template = preload("res://scripts/services/input/any_device.gd")
 
 var schemes = {
-    "selector" : {
+    "none" : {
         "keyboard" : self.keyboard_template.new(),
-        "any" : self.any_device_template.new(),
     }
 }
 
-var active_scheme = "selector"
+var active_scheme = "none"
 
 func _initialize():
-    self.load_basic_input()
+    self._load_basic_input()
 
 func handle_event(event):
     for device in self.schemes[self.active_scheme]:
@@ -27,6 +32,30 @@ func handle_event(event):
 func switch_to_scheme(scheme):
     self.active_scheme = scheme
 
-func load_basic_input():
-    self.schemes['selector']['keyboard'].register_handler(preload("res://scripts/services/input/handlers/quit.gd").new())
+func _load_basic_input():
+    self.register_handler('none', 'keyboard', preload("res://scripts/services/input/handlers/quit.gd").new())
 
+func create_scheme(name):
+    if not self.schemes.has(name):
+        self.schemes[name] = {}
+
+func register_device(scheme, device, device_id=0):
+    var device_instance = null
+
+    if device == self.DEVICE_KEYBOARD:
+        device_instance = self.keyboard_template.new()
+    elif device == self.DEVICE_GAMEPAD:
+        device_instance = self.gamepad_template.new(device_id)
+    elif device == self.DEVICE_MOUSE:
+        device_instance = self.mouse_template.new()
+    elif device == self.DEVICE_ARCADE:
+        device_instance = self.arcade_template.new()
+    elif device == self.DEVICE_ANY:
+        device_instance = self.any_device_template.new()
+
+    if not self.schemes[scheme].has(device) and device_instance != null:
+        self.schemes[scheme][device] = device_instance
+
+
+func register_handler(scheme, device, handler):
+    self.schemes[scheme][device].register_handler(handler)
