5a259373b202e27801126fbea65fd19d22c90643
CzlowiekImadlo <quak@tlen.pl>
2017-12-29 15:32:43 +0100

    Created base bag

--- scripts/bag.gd
+++ scripts/bag.gd
@@ -1,29 +1,11 @@
+extends "res://scripts/services/bag.gd"
 
-var root
-
-# SERVICES
-var file_handler = preload('res://scripts/services/file_handler.gd').new()
-var processing = preload('res://scripts/services/processing.gd').new()
-var timers = preload('res://scripts/services/timers.gd').new()
-var input = preload("res://scripts/input/input.gd").new()
-var sounds = preload("res://scripts/services/sounds.gd").new()
-
-# FUNCTIONALITIES
 var games = preload("res://scripts/functions/games.gd").new()
 var selector = preload("res://scripts/functions/selector.gd").new()
 var camera = preload("res://scripts/functions/camera.gd").new()
 
-func _init_bag(root):
-    self.root = root
-
-    self.processing._init_bag(self)
-    self.timers._init_bag(self)
-    self.input._init_bag(self)
-    self.sounds._init_bag(self)
-
+func _initialize():
     self.games._init_bag(self)
     self.selector._init_bag(self)
     self.camera._init_bag(self)
 
-func reset():
-    return
---
+++ scripts/services/bag.gd
@@ -0,0 +1,24 @@
+
+var root
+
+var file_handler = preload('res://scripts/services/file_handler.gd').new()
+var processing = preload('res://scripts/services/processing.gd').new()
+var timers = preload('res://scripts/services/timers.gd').new()
+var input = preload("res://scripts/input/input.gd").new()
+var sounds = preload("res://scripts/services/sounds.gd").new()
+
+func _init_bag(root):
+    self.root = root
+
+    self.processing._init_bag(self)
+    self.timers._init_bag(self)
+    self.input._init_bag(self)
+    self.sounds._init_bag(self)
+
+    self._initialize()
+
+func _initialize():
+    return
+
+func reset():
+    return
